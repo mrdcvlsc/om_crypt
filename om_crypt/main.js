351d9b5f210c6d5a3caca51b0bbcff402c679be8
Mordecai Velasco <59404502+mrdcvlsc@users.noreply.github.com>
2020-12-08 22:15:52 +0800

    Delete main.js

--- om_crypt/main.js
+++
@@ -1,9 +0,0 @@
-const om = require('./om_crypt.js');
-
-let msg = "this is my message";
-let emsg = om.encrypt(msg,"hiasd78d78g2ig3gydgtf76222e27ey2u82y786sed");
-let dmsg = om.decrypt(emsg,"hiasd78d78g2ig3gydgtf76222e27ey2u82y786sed");
-
-console.log(msg);
-console.log(emsg);
-console.log(dmsg);
